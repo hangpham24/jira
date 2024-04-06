2a38db49883d89fc6d9d1c8d79c1f712d364de98
Hang <hangnguyenpham2424@gmail.com>
2024-04-06 14:54:52 +0700

    add

---
+++ website_CLB_HTSV/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace website_CLB_HTSV.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
